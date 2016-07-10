71b7c19cbc0e037a7b97c26e70b0c06637df6697
lc <ziyu0123456789@gmail.com>
2016-07-10 22:40:35 +0800

    博客支持tasklist功能

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -10,6 +10,10 @@ gulp.task('css', () => {
     gulp.src(config.src.scss + '/**/*.?(s)css')
         .pipe($.print())
         .pipe($.sass())
+        .on('error', (err) => {
+        	// 这里捕获异常, 只是打印错误日志, 不会终止css任务
+            $.util.log(err);
+        })
         .pipe($.concat('style.css'))
         .pipe($.print())
         .pipe(gulp.dest(config.src.css));
