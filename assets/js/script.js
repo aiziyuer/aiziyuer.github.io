ba0128652eb135606060282c51a2dd5ef74e76d3
lc <ziyu0123456789@gmail.com>
2016-07-17 17:29:26 +0800

    博客增加评论功能

--- assets/js/script.js
+++ assets/js/script.js
@@ -148,22 +148,22 @@ function afterPjax() {
   });
 
   // Lazy Loading Disqus
-  // http://jsfiddle.net/dragoncrew/SHGwe/1/
-  // var ds_loaded = false,
-  //     top = $('#disqus_thread').offset().top;
-  // window.disqus_shortname = $('#disqus_thread').attr('name');
+  var ds_loaded = false;
+  var top = $('#disqus_thread').offset().top;
+  window.disqus_shortname = $('#disqus_thread').attr('name');
 
-  // function check() {
-  //   if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
-  //     $.ajax({
-  //       type: 'GET',
-  //       url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
-  //       dataType: 'script',
-  //       cache: true
-  //     });
-  //     ds_loaded = true;
-  //   }
-  // }check();
-  // container.scroll(check);
+  function check() {
+    if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
+      $.ajax({
+        type: 'GET',
+        url: '//' + disqus_shortname + '.disqus.com/embed.js',
+        dataType: 'script',
+        cache: true
+      });
+      ds_loaded = true;
+    }
+  }check();
+  container.scroll(check);
+
 }afterPjax();
 
